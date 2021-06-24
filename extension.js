4c353c59aa5978f42471d8d3c545fe561c381c49
Mina Kamel <minallkamel@gmail.com>
2021-06-24 02:00:09 +0200

    Turn on and off through prime-select

--- extension.js
+++ extension.js
@@ -20,7 +20,7 @@
 
 const GETTEXT_DOMAIN = 'my-indicator-extension';
 
-const { GObject, St } = imports.gi;
+const { GObject, St, Gio } = imports.gi;
 
 const Gettext = imports.gettext.domain(GETTEXT_DOMAIN);
 const _ = Gettext.gettext;
@@ -28,23 +28,112 @@ const _ = Gettext.gettext;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Util = imports.misc.util;
 
+// config
+const iconSize = 26;
+const primePath = '/usr/sbin/prime-select';
+
+// Todo: Get it to work with execCommunicate instead of Util.spawn, it ran it twice!
+
+// ##########################################################################
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
     _init() {
+        // Reference: https://github.com/eonpatapon/gnome-shell-extension-caffeine/blob/master/caffeine%40patapon.info/extension.js
         super._init(0.0, _('Switch Indicator'));
 
-        this.add_child(new St.Icon({
-            icon_name: 'face-smile-symbolic',
-            style_class: 'system-status-icon',
-        }));
+        this._settings = this.getSettings();
 
-        let item = new PopupMenu.PopupMenuItem(_('Show Notification'));
-        item.connect('activate', () => {
-            Main.notify(_('Whatʼs up, folks?'));
+        this._state = this.getStoredStatus();
+
+        if (this._state === true) {
+            primeLog('nvidia is On');
+        } else {
+            primeLog('intel is On');
+        }
+
+        let OnIconPath = `${Me.path}/icons/nvidia-on.png`;
+        let OffIconPath = `${Me.path}/icons/nvidia-off.png`;
+        
+        this._nvidiaOnIcon = Gio.icon_new_for_string(OnIconPath);
+        this._nvidiaOffIcon = Gio.icon_new_for_string(OffIconPath);
+        
+        this._icon = new St.Icon({
+            style_class: 'nvidia-icon',
+            icon_size: iconSize
         });
-        this.menu.addMenuItem(item);
+        
+        this._icon.gicon = this._state ? this._nvidiaOnIcon : this._nvidiaOffIcon;
+        this.add_actor(this._icon);
+        
+        this.connect('button-press-event', this.toggleState.bind(this));
+        
+        this.connect('touch-event', this.toggleState.bind(this));
+    }
+    
+    // Reference: http://justperfection.channel.gitlab.io/how-to-create-a-gnome-extension-documentation/Document.html#schema
+    getSettings() {
+        let GioSSS = Gio.SettingsSchemaSource;
+        let schemaSource = GioSSS.new_from_directory(
+            Me.dir.get_child("schemas").get_path(),
+            GioSSS.get_default(),
+            false
+            );
+            
+            let schemaObj = schemaSource.lookup(
+                'org.gnome.shell.extensions.prime', true);
+                
+                if (!schemaObj) {
+                    throw new Error('cannot find schemas');
+                }
+                
+                return new Gio.Settings({ settings_schema : schemaObj });
+            }
+            
+    toggleState() {
+        if (this._state === false) {
+            this.primeSelect('nvidia');
+            this._state = true;
+            this._icon.gicon = this._nvidiaOnIcon;
+            // ####################################
+            primeLog('Switching to nvidia..');
+            // ####################################
+            this.setCurrentProfile('nvidia');
+        } else if (this._state === true) {
+            this.primeSelect('intel');
+            this._state = false;
+            this._icon.gicon = this._nvidiaOffIcon;
+            // #####################################
+            primeLog('Switching to intel..');
+            // #####################################
+            this.setCurrentProfile('intel');
+        }
+        
+    }
+    
+    primeSelect(profile) {
+        let args = ['pkexec', primePath, profile];
+        Util.spawn(args);
+    }
+
+    getStoredStatus() {
+        let storedStatus = this._settings.get_string("current-profile");
+        if (storedStatus === 'intel') {
+            return false;
+        } else if (storedStatus === 'nvidia') {
+            return true;
+        }
+    }
+
+    setCurrentProfile(profile) {
+        this._settings.set_string("current-profile", profile);
+    }
+
+    notify(mesg) {
+        Main.notify(_(mesg));
+
     }
 });
 
@@ -68,4 +157,12 @@ class Extension {
 
 function init(meta) {
     return new Extension(meta.uuid);
+}
+
+function primeLog(logMesg) {
+    log(`[Prime-Switch-Log]: ${logMesg}`);
+}
+
+function primeLogErr(logMesg) {
+    throw logError(new Error(`[Prime-Switch-Error]: ${logMesg}`));
 }
